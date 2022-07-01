00a247a7288d5cd9cb0b58353857942f0616097e
Sunwoo <sw801733@naver.com>
2022-07-01 13:08:11 +0900

    alternavtive_method_commet

--- Silver_5/2869.cpp
+++ Silver_5/2869.cpp
@@ -14,6 +14,8 @@ int main()
     cin >> a >> b >> v;
 
     days = ceil((v - b) / (a - b));
+    // days = (v - b - 1) / (a - b) + 1; // 아예 목표에 도달하기 직전까지로 날짜를 계산 후 하루를 더해줌
+    
 
     cout << days;
 
