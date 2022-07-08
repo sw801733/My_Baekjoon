b27639e1a00d2f48f228985fb413bdb682ba33b3
Sunwoo <sw801733@naver.com>
2022-07-08 19:58:26 +0900

    compact_ver

--- Silver_5/1475.cpp
+++ Silver_5/1475.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <algorithm>
 
 using namespace std;
 
@@ -11,32 +11,26 @@ int main()
     cout.tie(NULL);
 
     int N;
-    int arr[10] = {0,0,0,0,0,0,0,0,0,0};
+    int arr[10] = {0,};
     int set = 0;
 
     cin >> N;
 
     while(N != 0)
     {
-        if (arr[N % 10] == 0)
-        {
-            if (N % 10 == 6 && arr[9] != 0)
-                arr[9]--;
-            else if (N % 10 == 9 && arr[6] != 0)
-                arr[6]--;
-            else
-            {
-                set++;
-                for(int i = 0; i < 10; i++)
-                    if (i != N % 10)
-                        arr[i]++;
-            }
-
-        }   
-        else
-            arr[N % 10]--;
+        arr[N % 10]++;
         N /= 10;
     }
+
+    for(int i = 0; i < 10; i++)
+    {
+        if (i == 6 || i == 9)
+            continue;
+        set = max(set,arr[i]);
+    }
+
+    set = max(set,((arr[6] + arr[9]) + 1) / 2);
+
     cout << set;
     return 0;
 }
