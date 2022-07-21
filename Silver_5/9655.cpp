d62a5f969128017b1dfb229aa9a6c129954c416f
Sunwoo <sw801733@naver.com>
2022-07-21 23:36:49 +0900

    dynamic_programming

--- Silver_5/9655.cpp
+++ Silver_5/9655.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,11 +8,20 @@ int main()
     int N;
     cin >> N;
 
-    if (N % 2 == 1)
+    int dp[1001];
+
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 1;
+
+    for(int i = 4; i <= N; i++)
+    {
+        dp[i] = min((dp[i - 1] + 1), (dp[i -3] + 1)); 
+    }    
+    if (dp[N] % 2 == 1)
         cout << "SK";
     else
         cout << "CY";
-    
 
     return 0;
 }
