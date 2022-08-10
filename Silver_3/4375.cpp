dd78ec21317ae9b3c45b2d0c39ae92c42a1a4582
Sunwoo <sw801733@naver.com>
2022-08-10 16:34:18 +0900

    math

---
+++ Silver_3/4375.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+    while(cin >> n)
+    {
+        int cnt = 1;
+        long long numberConsistOf1 = 1;
+        while(numberConsistOf1 % n != 0)
+        {
+            numberConsistOf1 = (numberConsistOf1 * 10) % n + 1;
+            cnt++;
+        }
+        cout << cnt << "\n";
+    }
+
+    return 0;
+}
