31fce3ebf20bb3b192c9239fc1d54944941c905d
sw801733 <84767822+sw801733@users.noreply.github.com>
2023-07-24 15:18:48 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    long long answer = 0;
+    
+    long long arr[2001] = {0,};
+    
+    arr[1] = 1;
+    arr[2] = 2;
+    
+    for(int i = 3; i <= n; i++)
+    {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+    }
+    
+    answer = arr[n];
+    return answer;
+}
