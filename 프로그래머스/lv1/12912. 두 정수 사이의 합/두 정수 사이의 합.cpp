7a255582c7e3c0adce4ab7c3b079caa2a15aaaee
sw801733 <84767822+sw801733@users.noreply.github.com>
2023-08-08 14:42:37 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    
+    answer = (long long)(a + b) * (abs(a - b) + 1) / 2;
+    
+    return answer;
+}
