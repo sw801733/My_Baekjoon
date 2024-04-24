bd0efbb4822da95a0e2d49a3322445f9bd7db558
sw801733 <84767822+sw801733@users.noreply.github.com>
2024-04-24 12:56:44 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(vector<int> ingredient) {
+    int answer = 0;
+    
+    vector<int> v = { -1 };
+    
+    for (int i : ingredient)
+    {
+        if (v.back() == 1 && i == 2)
+            v.back() = 12;
+        else if (v.back() == 12 && i == 3)
+            v.back() = 123;
+        else if (v.back() == 123 && i == 1)
+        {
+            answer++;
+            v.pop_back();
+        }
+        else
+            v.push_back(i);
+
+    }
+    
+    return answer;
+}
