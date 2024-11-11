c9609f2931ff3e5514e78736f62e2f7b321e3e2e
김선우 <84767822+sw801733@users.noreply.github.com>
2024-11-11 10:47:39 +0900

    [level 2] Title: H-Index, Time: 21.04 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        
+        for(int i = 0; i <= citations[citations.length - 1]; i++) {
+            int lessCnt = 0;
+            int moreCnt = 0;
+            for(int j = 0; j < citations.length; j++) {
+                if (citations[j] < i)
+                    lessCnt++;
+                if (citations[j] >= i)
+                    moreCnt++;
+            }
+            if (moreCnt < lessCnt)
+                break;
+            if (i <= moreCnt && lessCnt <= i)
+                answer = i;
+        }
+        
+        return answer;
+    }
+}
