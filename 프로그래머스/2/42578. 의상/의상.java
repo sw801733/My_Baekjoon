5dc43064f59a09d7262438473aa5e5d84c7c36f9
김선우 <84767822+sw801733@users.noreply.github.com>
2024-11-15 10:10:03 +0900

    [level 2] Title: 의상, Time: 0.32 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,18 +4,15 @@ class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> map = new HashMap<>();
 
-        // 각 종류별 옷 개수 저장
         for (String[] cloth : clothes) {
             map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
 
-        // 경우의 수 계산
         int answer = 1;
         for (int count : map.values()) {
-            answer *= (count + 1);  // 각 종류마다 입지 않는 경우를 포함해 경우의 수 계산
+            answer *= (count + 1);
         }
 
-        // 모든 옷을 입지 않는 경우 제외
         return answer - 1;
     }
 }
